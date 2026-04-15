accf7305d677d4a3745281c5386c7ba3c5b6e732
Sowndarya-D <sowndaryad59@gmail.com>
2026-04-15 12:32:33 +0530

    Initial commit

---
+++ 2varbl.py
@@ -0,0 +1,15 @@
+a=10
+b=23
+print("the value of a is:",a)
+print("the value of b is:",b)
+print("type of a is:",type(a) )
+
+c=3.12
+print("c is :",c)
+print("type of c is:",type(c) )
+
+#string
+d="python"
+print("d is:",d)
+print("type of d is:",type(d) )
+
---
+++ 3conditnstmt.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+
+print("The value of a is:", a)
+print("Type of a is:", type(a))
+if a > 10:
+    print("a is greater than 10")
+elif a == 10:
+    print("a is equal to 10")
+else:    print("a is less than 10")
